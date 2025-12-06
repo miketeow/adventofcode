d2f32d821f831ab6f0280adfd25473539922355f
miketeow <27071053+miketeow@users.noreply.github.com>
2025-12-06 22:39:20 +0800

    day1 js and rust done

--- 2025/day01/js/solution.js
+++ 2025/day01/js/solution.js
@@ -4,4 +4,28 @@ import path from "node:path";
 const inputPath = path.join(__dirname, "../input.txt");
 const input = fs.readFileSync(inputPath, "utf-8").trim();
 
-console.log(input);
+const array = input.split("\n");
+
+const main = (steps) => {
+  let anchor = 50;
+  let result = 0;
+
+  for (let i = 0; i < steps.length; i++) {
+    if (steps[i][0] === "L") {
+      anchor = anchor - (parseInt(steps[i].slice(1)) % 100);
+      if (anchor < 0) {
+        anchor = 100 + anchor;
+      }
+    } else {
+      anchor = (anchor + parseInt(steps[i].slice(1))) % 100;
+    }
+
+    if (anchor == 0) {
+      result++;
+    }
+  }
+
+  return result;
+};
+const result = main(array);
+console.log("The result is: " + result);
--- 2025/day01/rust/src/main.rs
+++ 2025/day01/rust/src/main.rs
@@ -1,8 +1,38 @@
-use std::fs;
-
 fn main() {
-  let input = fs::read_to_string("../input.txt").expect("Error reading input.txt");
-    println!("--- Rust Output ---");
-    println!("{}", input);
-    println!("--- End of Rust Output ---");
+  // let input = fs::read_to_string("../sampleinput.txt").expect("Error reading input.txt");
+  let input: &str = include_str!("../../input.txt");
+  let mut start: i32 = 50;
+  let mut result: i32 = 0;
+  for line in input.lines(){
+    // println!("Direction: {}, Number: {}", line.chars().next().unwrap(), &line[1..]);
+    if line.chars().next().unwrap() == 'R' {
+      start = (start + &line[1..].parse::<i32>().unwrap()) % 100;
+    } else {
+      start = start - (&line[1..].parse::<i32>().unwrap() % 100);
+      if start < 0{
+        start = 100 + start;
+      }
+    }
+    if start == 0 {
+      result += 1;
+    }
+  }
+
+  println!("The result is: {}", result);
+  println!("The result using the functionis: {}", solve(input));
+}
+
+fn solve(input: &str) -> i32 {
+  let mut start: i32 = 50;
+  let mut result: i32 = 0;
+
+  for line in input.lines() {
+    let num = line[1..].parse::<i32>().unwrap();
+    let move_amount = if line.starts_with('R') {num} else {-num};
+    start = (start + move_amount).rem_euclid(100);
+    if start == 0 {
+      result += 1;
+    }
+  }
+  result
 }
